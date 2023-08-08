51ca11b944820650dd38485e2c9595b4d0815bd9
kangyongwei <kangyongwei@mediatrack.cn>
2023-08-08 18:30:48 +0800

    update

--- config.js
+++ config.js
@@ -1,45 +1,46 @@
+config.js; // 北为正值,南为负值; 东为正值,西为负值
 window.config = {
   莫问今朝: {
     updated_at: '8月8日 14:39',
     resource: [
       {
-        point: [-764, -484],
+        point: [-764, -484], // 西764, 南484
         type: 'treasure',
         tooltip: '营地内',
       },
       {
-        point: [207, -1012],
+        point: [207, -1012], // 东207, 南1012
         type: 'treasure',
         tooltip: '树枝上',
       },
       {
-        point: [1913, -1328],
+        point: [1913, -1328], // 东1913, 南1328
         type: 'treasure',
         tooltip: '山头上',
       },
       {
-        point: [1121, 4],
+        point: [1121, 4], // 东1121, 北4
         type: 'treasure',
         tooltip: '半山腰',
       },
       {
-        point: [877, -1611],
+        point: [877, -1611], // 东877, 南1611
         type: 'pig',
       },
       {
-        point: [988, -875],
+        point: [988, -875], // 东988, 南875
         type: 'pig',
       },
       {
-        point: [960, -480],
+        point: [960, -480], // 东960, 南480
         type: 'bean',
       },
       {
-        point: [1066, -484],
+        point: [1066, -484], // 东1066, 南484
         type: 'bean',
       },
       {
-        point: [859, -1836],
+        point: [859, -1836], // 东859, 南1836
         type: 'salt',
         tooltip: '两个在一起',
       },
@@ -49,43 +50,43 @@ window.config = {
     updated_at: '8月8日 15:50',
     resource: [
       {
-        point: [1133, -258],
+        point: [1133, -258], // 东1133, 南258
         type: 'bean',
       },
       {
-        point: [887, -318],
+        point: [887, -318], // 东887, 南318
         type: 'bean',
       },
       {
-        point: [845, -1840],
+        point: [845, -1840], // 东845, 南1840
         type: 'salt',
       },
       {
-        point: [488, -1969],
+        point: [488, -1969], // 东488, 南1969
         type: 'salt',
       },
       {
-        point: [1103, -1375],
+        point: [1103, -1375], // 东1103, 南1375
         type: 'pig',
       },
       {
-        point: [1259, -624],
+        point: [1259, -624], // 东1259, 南624
         type: 'pig',
       },
       {
-        point: [1946, -708],
+        point: [1946, -708], // 东1946, 南708
         type: 'treasure',
       },
       {
-        point: [1913, -1329],
+        point: [1913, -1329], // 东1913, 南1329
         type: 'treasure',
       },
       {
-        point: [-1150, -1406],
+        point: [-1150, -1406], // 西1150, 南1406
         type: 'treasure',
       },
       {
-        point: [-1612, -1512],
+        point: [-1612, -1512], // 西1612, 南1512
         type: 'treasure',
       },
     ],
