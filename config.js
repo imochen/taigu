9e1c914c53ff21c3d01925297ab93ff9336ff9b0
kangyongwei <kangyongwei@mediatrack.cn>
2023-08-08 18:15:42 +0800

    add config.js

---
+++ config.js
@@ -0,0 +1,93 @@
+window.config = {
+  莫问今朝: {
+    updated_at: '8月8日 14:39',
+    resource: [
+      {
+        point: [-764, -484],
+        type: 'treasure',
+        tooltip: '营地内',
+      },
+      {
+        point: [207, -1012],
+        type: 'treasure',
+        tooltip: '树枝上',
+      },
+      {
+        point: [1913, -1328],
+        type: 'treasure',
+        tooltip: '山头上',
+      },
+      {
+        point: [1121, 4],
+        type: 'treasure',
+        tooltip: '半山腰',
+      },
+      {
+        point: [877, -1611],
+        type: 'pig',
+      },
+      {
+        point: [988, -875],
+        type: 'pig',
+      },
+      {
+        point: [960, -480],
+        type: 'bean',
+      },
+      {
+        point: [1066, -484],
+        type: 'bean',
+      },
+      {
+        point: [859, -1836],
+        type: 'salt',
+        tooltip: '两个在一起',
+      },
+    ],
+  },
+  血战: {
+    updated_at: '8月8日 15:50',
+    resource: [
+      {
+        point: [1133, -258],
+        type: 'bean',
+      },
+      {
+        point: [887, -318],
+        type: 'bean',
+      },
+      {
+        point: [845, -1840],
+        type: 'salt',
+      },
+      {
+        point: [488, -1969],
+        type: 'salt',
+      },
+      {
+        point: [1103, -1375],
+        type: 'pig',
+      },
+      {
+        point: [1259, -624],
+        type: 'pig',
+      },
+      {
+        point: [1946, -708],
+        type: 'treasure',
+      },
+      {
+        point: [1913, -1329],
+        type: 'treasure',
+      },
+      {
+        point: [-1150, -1406],
+        type: 'treasure',
+      },
+      {
+        point: [-1612, -1512],
+        type: 'treasure',
+      },
+    ],
+  },
+};
