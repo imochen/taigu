b7a951567f7d329327448d8f01863c7b6d90ac0a
mochen.china <mochen.china@gmail.com>
2023-08-13 09:53:39 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,173 +1,173 @@
 // 北为正值,南为负值; 东为正值,西为负值
 window.config = {
   莫问今朝: {
-    updated_at: '8月12日',
+    updated_at: '8月13日',
     resource:[
-      {
-        "point": [
-          600,
-          -1751
-        ],
-        "type": "treasure",
-        "tooltip": ""
-      },
-      {
-        "point": [
-          1396,
-          235
-        ],
-        "type": "treasure",
-        "tooltip": ""
-      },
-      {
-        "point": [
-          1260,
-          -624
-        ],
-        "type": "pig",
-        "tooltip": ""
-      },
-      {
-        "point": [
-          985,
-          -875
-        ],
-        "type": "pig",
-        "tooltip": ""
-      },
-      {
-        "point": [
-          1136,
-          -258
-        ],
-        "type": "bean",
-        "tooltip": ""
-      },
-      {
-        "point": [
-          1141,
-          -387
-        ],
-        "type": "bean",
-        "tooltip": ""
-      },
-      {
-        "point": [
-          563,
-          -1774
-        ],
-        "type": "salt",
-        "tooltip": ""
-      },
-      {
-        "point": [
-          877,
-          -1828
-        ],
-        "type": "salt",
-        "tooltip": ""
-      },
-      {
-        "point": [
-          -1150,
-          -1405
-        ],
-        "type": "treasure",
-        "tooltip": ""
-      },
-      {
-        "point": [
-          312,
-          -607
-        ],
-        "type": "treasure",
-        "tooltip": ""
-      }
+  {
+    "point": [
+      -1340,
+      -353
     ],
+    "type": "treasure",
+    "tooltip": ""
+  },
+  {
+    "point": [
+      336,
+      -1547
+    ],
+    "type": "treasure",
+    "tooltip": ""
+  },
+  {
+    "point": [
+      1466,
+      -799
+    ],
+    "type": "treasure",
+    "tooltip": ""
+  },
+  {
+    "point": [
+      1396,
+      235
+    ],
+    "type": "treasure",
+    "tooltip": ""
+  },
+  {
+    "point": [
+      1059,
+      -1041
+    ],
+    "type": "pig",
+    "tooltip": ""
+  },
+  {
+    "point": [
+      727,
+      -1525
+    ],
+    "type": "pig",
+    "tooltip": ""
+  },
+  {
+    "point": [
+      959,
+      -477
+    ],
+    "type": "bean",
+    "tooltip": ""
+  },
+  {
+    "point": [
+      1173,
+      -467
+    ],
+    "type": "bean",
+    "tooltip": ""
+  },
+  {
+    "point": [
+      826,
+      -1831
+    ],
+    "type": "salt",
+    "tooltip": ""
+  },
+  {
+    "point": [
+      486,
+      -1970
+    ],
+    "type": "salt",
+    "tooltip": ""
+  }
+],
   },
   血战: {
-    updated_at: '8月12日',
+    updated_at: '8月13日',
     resource: [
-      {
-        "point": [
-          830,
-          -380
-        ],
-        "type": "bean",
-        "tooltip": ""
-      },
-      {
-        "point": [
-          1115,
-          -508
-        ],
-        "type": "bean",
-        "tooltip": ""
-      },
-      {
-        "point": [
-          873,
-          -1830
-        ],
-        "type": "salt",
-        "tooltip": ""
-      },
-      {
-        "point": [
-          653,
-          -1778
-        ],
-        "type": "salt",
-        "tooltip": ""
-      },
-      {
-        "point": [
-          879,
-          -1607
-        ],
-        "type": "pig",
-        "tooltip": ""
-      },
-      {
-        "point": [
-          1442,
-          -520
-        ],
-        "type": "pig",
-        "tooltip": ""
-      },
-      {
-        "point": [
-          -1340,
-          -352
-        ],
-        "type": "treasure",
-        "tooltip": ""
-      },
-      {
-        "point": [
-          -155,
-          -807
-        ],
-        "type": "treasure",
-        "tooltip": ""
-      },
-      {
-        "point": [
-          930,
-          -1090
-        ],
-        "type": "treasure",
-        "tooltip": ""
-      },
-      {
-        "point": [
-          675,
-          -272
-        ],
-        "type": "treasure",
-        "tooltip": ""
-      }
+  {
+    "point": [
+      -375,
+      25
     ],
+    "type": "treasure",
+    "tooltip": ""
+  },
+  {
+    "point": [
+      -155,
+      -807
+    ],
+    "type": "treasure",
+    "tooltip": ""
+  },
+  {
+    "point": [
+      1626,
+      -1138
+    ],
+    "type": "treasure",
+    "tooltip": ""
+  },
+  {
+    "point": [
+      675,
+      -272
+    ],
+    "type": "treasure",
+    "tooltip": ""
+  },
+  {
+    "point": [
+      1172,
+      -467
+    ],
+    "type": "bean",
+    "tooltip": ""
+  },
+  {
+    "point": [
+      1136,
+      -264
+    ],
+    "type": "bean",
+    "tooltip": ""
+  },
+  {
+    "point": [
+      1033,
+      -2105
+    ],
+    "type": "salt",
+    "tooltip": ""
+  },
+  {
+    "point": [
+      1012,
+      -1631
+    ],
+    "type": "salt",
+    "tooltip": ""
+  },
+  {
+    "point": [
+      1374,
+      -757
+    ],
+    "type": "pig",
+    "tooltip": ""
+  },
+  {
+    "point": [
+      852,
+      -536
+    ],
+    "type": "pig",
+    "tooltip": ""
+  }
+],
   },
 };
